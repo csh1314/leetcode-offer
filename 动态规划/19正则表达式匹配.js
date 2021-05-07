bf8d126f8d2f5cc4932b4b10db1fb601513c25b5
csh1314 <1622135447@qq.com>
2021-05-07 08:54:41 +0800

    add 14 II、19 进度(75/75)

--- 动态规划/19正则表达式匹配.js
+++ 动态规划/19正则表达式匹配.js
@@ -7,11 +7,29 @@
  * 规则：'.'匹配任意字符,'*'表匹配前一个字符0或多个
  */
  var isMatch = function(s, p) {
-    const target = s.split('');
-    let dp = p.split('');
-    // for(let i =0; i< target.length ; i++){
-    //     if(dp[i] === target[i])
-    //         continue;
-    // }
-    // return false;
+    // return new RegExp('^'+p+'$').test(s)
+    if(s == null || p == null) return false
+    const slen = s.length, plen = p.length
+    const dp = new Array(slen+1)
+    for(let i = 0; i<dp.length; i++)
+        dp[i] = new Array(plen + 1).fill(false)
+    dp[0][0] = true
+    // 初始化首行
+    for(let j = 1; j<plen+1; j++)
+        if(p[j-1] == '*')
+            dp[0][j] = dp[0][j-2]
+    // 迭代
+    for(let i = 1; i<slen+1; i++){
+        for(let j = 1; j<plen+1; j++){
+            if(s[i-1] == p[j-1] || p[j-1] == '.')
+                dp[i][j] = dp[i-1][j-1]
+            else if(p[j-1] == '*'){
+                if(s[i-1] == p[j-2] || p[j-2] == '.')
+                    dp[i][j] = dp[i][j-2] || dp[i-1][j-2] || dp[i-1][j]
+                else
+                    dp[i][j] = dp[i][j-2]
+            }   
+        }
+    }
+    return dp[slen][plen]
 };
--- 数学/14- II剪绳子 II.js
+++ 数学/14- II剪绳子 II.js
@@ -2,6 +2,17 @@
  * @param {number} n
  * @return {number}
  */
- var cuttingRope = function(n) {
-
+// 相较于 剪绳子I 要做大数求余处理
+// 核心仍为 算术平均根 >= 几何平均根 
+var cuttingRope = function(n) {
+    const arr = [0,0,1,2,4]
+    if(n < 5)
+        return arr[n]
+    const mod = Math.pow(10,9) + 7
+    let ans = 1
+    while(n >= 5){
+        ans = ans%mod * 3
+        n -= 3
+    }
+    return ans*n%mod
 };
